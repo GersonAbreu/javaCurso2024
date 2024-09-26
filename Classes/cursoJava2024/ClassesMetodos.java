03c0e2b1c9c2e1144e59452be6cf1f2687852163
Gerson Abreu <ge.parma@gmail.com>
2024-09-26 18:54:49 -0300

    Usando Metodos

--- Classes/cursoJava2024/ClassesMetodos.java
+++ Classes/cursoJava2024/ClassesMetodos.java
@@ -27,6 +27,17 @@ public class ClassesMetodos {
 			return numero * fatorial(numero - 1);
 		}
 	
+	//metodo 5: encontra o maior numero em um array:
+	public int maiorNumero(int[]numeros) {
+		int maior = numeros[0];
+		for(int numero :numeros) {
+			if (numero > maior ) {
+				maior = numero;
+			}
+		}
+			return maior;
+	}
+	
 
 	// Este metodo executa outros metodos!
 	public static void main(String[] args) {
@@ -35,22 +46,32 @@ public class ClassesMetodos {
 		// o metodo ou acrescentar classe.:
 		ClassesMetodos util = new ClassesMetodos();
 
-		// Chamando o Método soma:
+		//1. Chamando o Método soma :
 		System.out.println("Soma de 5 e 3: " + util.soma(5, 3));
-		// chamando o metodo par, para saber se o metodo soma é par
+		
+		//2. chamando o metodo par, para saber se o metodo soma é par 
 		System.out.println("O numero da soma acima é: " + util.par(util.soma(5, 3)));
 
-		// chamando metodo para inverter string
+		//3. chamando metodo para inverter string 
 		// variavel para ser invertida
 		String nome = "Gerson";
-		// Chamada do método inverter
+		// Chamada do método inverter 
 		String invertida = util.inverter(nome);
 
 		System.out.println("O nome é: " + nome + " e invertido fica: " + invertida);
 
 
-		// Exibe o resultado
+		// 4. Exibe o resultado de fatorial
 		System.out.println("O fatorial de 5  é: " + util.fatorial(5));
+		
+		//5. exibe o numero maior
+		int [] numeros = {1,5,6,8,44};
+		System.out.println("O maior numero do Array é: "+ util.maiorNumero(numeros));
+		
+		
+		
+		
+		
 
 	}
 
