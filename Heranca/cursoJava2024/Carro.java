5bb0bdeb15f43a0bd62addc0cda9abc15ca03da1
Gerson Abreu <ge.parma@gmail.com>
2024-10-01 21:35:59 -0300

    Herança v1

--- Heranca/cursoJava2024/Carro.java
+++ Heranca/cursoJava2024/Carro.java
@@ -1,9 +1,10 @@
 package cursoJava2024;
 
-public class Carro extends Veiculo {
-	private int numeroDePortas;
+public class Carro extends Veiculo { //pega os atributos e os metodos da classe Veiculo
+	private int numeroDePortas; //private: diz que a variável só pode ser usada na própria classe onde foi declarada
 	
-	public Carro(String marca, String modelo, int ano, int numeroDePortas) {
+	// metodo public Carro é classe construtora// as variaveis estão tipadas por String e int
+	public Carro(String marca, String modelo, int ano, int numeroDePortas) { 
 		super (marca, modelo, ano);
 		this.numeroDePortas= numeroDePortas;
 	}
--- Heranca/cursoJava2024/TesteVeiculo.java
+++ Heranca/cursoJava2024/TesteVeiculo.java
@@ -4,9 +4,23 @@ public class TesteVeiculo {
 
 	public static void main(String[] args) {
 		Carro carro = new Carro("Toyota", "Corola", 2004, 4);
+		Carro carro1 = new Carro("BMW", "z4", 2022, 4);
+		Moto moto = new Moto ("Honda","CB500",2019, true);
+		Moto moto1 = new Moto ("Honda","x3",2025, false);
+		
+
 
 		System.out.println("Detalhes do Carro: ");
 		carro.exibirDetalhes();
+		
+		System.out.println("\nDetalhes da Moto: ");
+		moto.exibirDetalhes();
+		
+		System.out.println("\nDetalhes do Carro: ");
+		carro1.exibirDetalhes();
+		
+		System.out.println("\nDetalhes da Moto: ");
+		moto1.exibirDetalhes();
 		
 		
 	}
